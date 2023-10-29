c8a1ac7cc7707db5895357ab2fd340e05128e2d0
FrankWuensch <wuensch.frank@web.de>
2023-10-29 13:19:38 +0100

    readded deploy files

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:FrankWuensch/klangkunst.git master:gh-pages
+git push -f git@github.com:FrankWuensch95/klangkunst.git main:gh-pages
 
 cd -
